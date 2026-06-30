9600e9a758492bce99a47c3b4dec235010fee535
MDGreece <mythicdungeongreece@gmail.com>
2026-06-30 16:37:07 +0200

    Improve formatting of guild table rows
    Refactor HTML string for better readability.

--- guilds.js
+++ guilds.js
@@ -2,7 +2,7 @@ fetch("./data/guilds.json")
   .then(response => response.json())
   .then(guilds => {
     const tableBody = document.getElementById("guildTableBody");
-    
+
     guilds.forEach(guild => {
       let rankDisplay = guild.rank;
 
@@ -12,16 +12,17 @@ fetch("./data/guilds.json")
 
       const row = document.createElement("tr");
 
-   row.innerHTML = `
-  <td>${rankDisplay}</td>
-  <td>
-    <a class="guild-link" href="guild.html?id=${guild.id}">
-      ${guild.name}
-    </a>
-  </td>
-  <td>${guild.worldRank}</td>
-  <td>${guild.progress}</td>
-`;
+      row.innerHTML = `
+        <td>${rankDisplay}</td>
+        <td>
+          <a class="guild-link" href="guild.html?id=${guild.id}">
+            ${guild.name}
+          </a>
+        </td>
+        <td>${guild.worldRank}</td>
+        <td>${guild.progress}</td>
+      `;
+
       tableBody.appendChild(row);
     });
   });
