5d83e1f143f698a500dda53d8c08f8beeb32c3df
MDGreece <mythicdungeongreece@gmail.com>
2026-08-20 16:30:44 +0200

    Change CURRENT_RAID_KEY to 'venomous-abyss'

--- scripts/update-warcraftlogs.js
+++ scripts/update-warcraftlogs.js
@@ -8,7 +8,7 @@ const outputPath = "data/warcraftlogs-groups.json";
 
 const TOTAL_BOSSES = 8;
 const REPORT_LIMIT = 50;
-const CURRENT_RAID_KEY = "midnight-tier-1";
+const CURRENT_RAID_KEY = "venomous-abyss";
 
 /*
  * Canonical encounter order.
